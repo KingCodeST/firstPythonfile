542c7cf862e2a16536305440f43806710948e596
cyfotyasi <Tya@2017>
2019-06-01 00:11:53 +0200

    commit pracitse

---
+++ app.py
@@ -0,0 +1,45 @@
+# print("Hello world ")
+#
+#
+# print(" /   |")
+# print("/____|")
+# print("    /|")
+# print("   / |")
+# print("  /  |")
+
+# character_name = "John"
+# character_age =60
+# number_age =54
+#
+# print("There once was a man named "+character_name+ "," )
+# print("He was "+number_age+ " years old.")
+#
+# character_name ="Michael"
+# character_name =7
+#
+# print("He really liked the name "+character_name+ ", ")
+# print("He really didnt like being "+character_age+ "")
+#
+
+
+
+#String working with
+
+# phrase ="Giraffe Academy"
+#
+# print(phrase+" is Cool ")
+# print(phrase.lower())
+# print(phrase.upper())
+# print(phrase.upper().isupper())
+# print(len(phrase))
+# print(phrase[0].lower())
+# print(phrase.index("f"))
+# print(phrase.replace("Giraffe "," Elephant "))
+
+
+#numbers
+
+print(2)
+print(-2.68474)
+print(4+7)
+print(10%3)
