3c33d7154534c89f058bce13a1a10bec39d64000
cyfotyasi <Tya@2017>
2019-06-04 16:41:54 +0200

    commit pracitse

--- app.py
+++ app.py
@@ -38,8 +38,95 @@
 
 
 #numbers
+#
+# print(2)
+# print(-2.68474)
+# print(4+7)
+# print(10%3)
+# my_num = 5
+# print(str(my_num )+" My favorite number")
+#
+# my_nam = -4
+#
+# print(my_nam)
+#
+# print(round(4.9))
+#
+from math import *
+#
+#
+# print(floor(3.9))
+# print(ceil(3.9))
+# print(sqrt(90))
+#
 
-print(2)
-print(-2.68474)
-print(4+7)
-print(10%3)
+
+# input from user
+
+
+# name = input("Enter Your Name: ")
+#
+# age = input("Enter Your Age: ")
+#
+# print("Hello "+ name+ "!")
+#
+# print("Your age is "+ age+ "!")
+#
+
+# create calculate
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter a Another number: ")
+# result = float(num1) + float(num2)
+#
+# print(result)
+#
+
+# mad libs game
+# color =input("Enter a color")
+# pluralnoun =input("Enter a color")
+# celebrity =input("Enter a celebrate")
+#
+#
+# print("Roses are "+ color )
+# print(pluralnoun+" are blue")
+# print("I love "+celebrity)
+
+#
+# friends = ["Siya ","Karren","Jim","Lungisa","Powlo"]
+# friends[1] ="mike"
+#
+# print(friends[1:4])
+
+# lucky_numbers =[4,8,15,16,23,42]
+# friends =["sibonigle","Snalo","Nobuhle","Nolthando","Siya","Myathaza","Sine"]
+
+#friends.append("creed")
+
+# friends.insert(1,"Kelly")
+
+#friends.remove()
+# friends.pop(5)
+#
+# print(friends.index("Siya"))
+#
+# friends.sort()
+
+#tuple
+
+# coordinates =(4,5)
+#
+#
+# print(coordinates[0])
+#
+#
+
+#function
+
+def say_hi(name,age):
+    print("Hello "+name)
+
+print("Top")
+say_hi("Siphokuhle","35")
+print("Buttom")
+
